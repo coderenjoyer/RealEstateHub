b303e38342539cea42978f6d66092069467b3583
renz <renzukki@gmail.com>
2025-10-01 22:54:17 +0800

    Login page (parent container and modal)

--- AzureConnect/src/App.tsx
+++ AzureConnect/src/App.tsx
@@ -1,25 +1,18 @@
-import { Header } from "./components/landing/header"
-import { HeroSection } from "./components/landing/hero-section"
-import { ServicesSection } from "./components/landing/services-section"
-import { WhyChooseSection } from "./components/landing/why-choose-section"
-import { ReviewsSection } from "./components/landing/reviews-section"
-import { CTASection } from "./components/landing/cta-section"
-import { Footer } from "./components/landing/footer"
-import LoginModal from "./components/login/login_modal"
+// import { Header } from "./components/landing/header"
+// import { HeroSection } from "./components/landing/hero-section"
+// import { ServicesSection } from "./components/landing/services-section"
+// import { WhyChooseSection } from "./components/landing/why-choose-section"
+// import { ReviewsSection } from "./components/landing/reviews-section"
+// import { CTASection } from "./components/landing/cta-section"
+// import { Footer } from "./components/landing/footer"
+import LoginParentContainer from "./components/login/login_parent_container";
 
 function App() {
   return (
     <main className="min-h-screen">
-      <LoginModal />
-      <Header />
-      <HeroSection />
-      <ServicesSection />
-      <WhyChooseSection />
-      <ReviewsSection />
-      <CTASection />
-      <Footer />
+      <LoginParentContainer />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
